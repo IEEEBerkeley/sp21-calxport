1ee303f570fa85d468efb1433aa8a563e6b07783
khoi <khoing0810@berkeley.edu>
2021-04-04 08:02:11 -0700

    working on sending and listening message between popup.js and contentScripts.js. tasks included in popup.js

--- contentScripts.js
+++ contentScripts.js
@@ -1,1 +1,3 @@
 console.log("contentScripts.js running...")
+var firstCourseName = document.getElementsByClassName("ps_grid-cell$0");
+  console.log(firstCourseName)
--- popup.js
+++ popup.js
@@ -1,12 +1,15 @@
 let alertButton = document.getElementById('getCourse');
-const MutationObserver = window.MutationObserver;
-// const observer = new MutationObserver(function(mutations) {
-//   mutations.forEach(function(mutation) {
-
-//   })
-// })
+// tasks: 
+// 1) find out how to get chrome.tabs element (query) to return current tab info (url, etc.) 
+//-> possible issue: chrome.tabs only runs in popup.html environment -> it's not a tab and thus not
+// returning the current tab's info
+// 2) Get popup.js send message to contentScripts.js whenever the user clicks on "Grab classes!" (for now)
 alertButton.addEventListener("click", function () {
-  var firstCourseName = document.getElementsByClassName("ps_grid-cell$0");
-  console.log(firstCourseName)
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+  //     console.log(response.farewell);
+  //   });
+  // });
+  console.log(chrome.tabs);
   console.log("clicked on grab classes!")
 }); 
