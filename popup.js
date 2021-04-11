c587c4729d2e45b7a02438477cadb881800c434a
khoi <khoing0810@berkeley.edu>
2021-04-10 20:44:00 -0700

    changed manifest version to V3

--- popup.js
+++ popup.js
@@ -5,22 +5,17 @@ let exportButton = document.getElementById('export');
 //-> possible issue: chrome.tabs only runs in popup.html environment -> it's not a tab and thus not
 // returning the current tab's info
 // 2) Get popup.js send message to contentScripts.js whenever the user clicks on "Grab classes!" (for now)
-getCourseButton.addEventListener("click", function () {
-  // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  //   console.log(tabs);
-  //   console.log(chrome.runtime);
-  //   if (!tabs[0].url.includes("https://bcsweb.is.berkeley.edu/")) {
-  //     alert("Invalid site. Please go to CalCentral's Enrollment Center.");
-  //     return;
-  //   }
-  //   chrome.tabs.sendMessage(tabs[0].id, {msg: "grab"}, function(response) {
-  //     console.log(response.receivedStatus);
-  //   });
-  // });
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    console.log(tabs);
-    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-      console.log(response.farewell);
-    });
-  });
+getCourseButton.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (!tab.url.includes("bcsweb.is.berkeley.edu")) {
+    alert("Invalid site. Please go to CalCentral's Enrollment Center.");
+    return
+  }
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: injectScripts,
+  })
 }); 
+function injectScripts() {
+  console.log("hi");
+}
