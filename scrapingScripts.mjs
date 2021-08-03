c6c3c8d5626240cea1776619a9f39a65651cb141
khoi <khoing0810@berkeley.edu>
2021-08-02 21:01:43 -0700

    moved scraping function to separated module js file

---
+++ scrapingScripts.mjs
@@ -0,0 +1,136 @@
+export function scrapedData() {
+    var courseList = [];
+    /** NOTE: need to replace these codes with function calls for the simplicity of codes (currently unable to do so) */
+    function getCourseName(courseIdx) {
+      /** Grabs course name HTML element*/
+      var courseHTMLElem = document.getElementById(`DERIVED_SSR_FL_SSR_SCRTAB_DTLS$${courseIdx}`);
+      return courseHTMLElem.innerText;
+    }
+  
+    function getSectionName(node) {
+      var sectionName = getElementStringInRow(node, 0)
+      var splitArray = sectionName.split(" - ");
+      return splitArray[0];
+    }
+  
+    /** Returns an array of start and end dates 
+     * Index 0: start date
+     * Index 1: end date
+     */
+    function getStartEndDates(node) {
+      var dates = getElementStringInRow(node, 1);
+      var splitArray = dates.split(" - ");
+      return [splitArray[0], splitArray[1]];
+    }
+  
+    function formatStartEndDates(arr) {
+      var startDate = arr[0].split("/").join("-");
+      var endDate = arr[1].split("/").join("-");
+      return [startDate, endDate]
+    }
+  
+    /** Returns an array of days and times of a class
+     * Index 0: array of all the days of the class
+     * Index 1: array of start time (index 0) and end time (index 1)
+     * Returns either nested arrays, a single array of strings, or null
+     */
+    function getDaysTimes(node) {
+      // Can handle special cases
+      var dtString = getElementStringInRow(node, 2);
+      //console.log(dtString);
+      var dtArray = dtString.split("\n");
+      console.log(dtArray);
+      if (dtArray.length > 2) {
+        return dtArray;
+      }
+      return processDayTimeArray(dtArray);
+    }
+  
+    function processDayTimeArray(arr) {
+      // Only handles normal cases + TBA cases
+      //"Schedule: To Be Announced case"
+      if (arr.length == 1) {
+        return null;
+      }
+  
+      var dayArray = arr[0].split(" ");
+      var timeArray = arr[1].split(" ");
+      dayArray.shift();
+      timeArray.shift();
+      if (dayArray.join(" ") == "To be Announced") {
+        return null;
+      }
+      if (timeArray.join(" ") == "To be Announced") {
+        return null;
+      }
+      timeArray = [timeArray[0], timeArray[2]]
+      return [dayArray, timeArray]
+    }
+  
+    function formatTime(timeString) {
+      if (timeString.includes("AM")) {
+    
+        var onlyTime = timeString.substring(0, timeString.indexOf("AM")) + ":00";
+        var splitArray = onlyTime.split(":");
+        if (splitArray[0] == "12") {
+          splitArray[0] = "00";
+        }
+        return splitArray.join(":");
+      } else {
+        var onlyTime = timeString.substring(0, timeString.indexOf("PM")) + ":00";
+        var splitArray = onlyTime.split(":");
+        if (splitArray[0] != "12") {
+          var hour = parseInt(splitArray[0]);
+          splitArray[0] = `${hour + 12}`;
+        }
+        return splitArray.join(":");
+      }
+    }
+    
+    function getRoom(node) {
+      return getElementStringInRow(node, 3);
+    }
+  
+    /** Get specific element in the course row (in course table) */
+    function getElementStringInRow(parentNode, gridCellIdx) {
+      var element = document.getElementById(parentNode).getElementsByClassName("ps_grid-cell")[gridCellIdx].innerText;
+      return element;
+    }
+  
+    /** Get course name and all related sections and their info */
+    function getCourseInfo(courseIdx, specialCaseArray=[]) {
+      var courseInfo = {};
+      for (var row = 0; document.getElementById(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`) != null; row += 1) {
+        var sectionInfo = {};
+        var section =  getSectionName(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`);
+        
+        //ADD WARNING TO USER FOR IT
+        if (getDaysTimes(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`) == null) {
+          continue;
+        }
+        if (getDaysTimes(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`).length > 2) {
+          // ignore incorrect cases
+          continue;
+        }
+        sectionInfo["course"] = getCourseName(courseIdx);
+        sectionInfo["section"] = section;
+        sectionInfo["startDate"] = formatStartEndDates(getStartEndDates(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`))[0];
+        sectionInfo["endDate"] = formatStartEndDates(getStartEndDates(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`))[1].trim();
+        sectionInfo["days"] = getDaysTimes(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`)[0];
+        sectionInfo["startTime"] = formatTime(getDaysTimes(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`)[1][0]);
+        sectionInfo["endTime"] = formatTime(getDaysTimes(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`)[1][1]);
+        sectionInfo["room"] = getRoom(`STDNT_ENRL_SSVW$${courseIdx}_row_${row}`).trim();
+        courseInfo[`${section}`] = sectionInfo
+      }
+      return courseInfo;
+    }
+    for (var i = 0; document.getElementById(`DERIVED_SSR_FL_SSR_SCRTAB_DTLS$${i}`) != null; i += 1) {
+      /** Ignores dropped classes */
+      if (document.getElementById(`DERIVED_SSR_FL_SSR_DRV_STAT$392$$${i}`).innerText == "Dropped") {
+        continue;
+      }
+      courseList.push(getCourseInfo(i));
+    }
+    console.log(courseList);
+    return courseList;
+}
